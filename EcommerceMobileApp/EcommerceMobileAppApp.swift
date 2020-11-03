b04a2401e69ddfa1c9edf05fc7b2ae4d9afe9287
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-11-03 19:22:45 -0300

    Initial Commit

---
+++ EcommerceMobileApp/EcommerceMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceMobileAppApp.swift
+//  EcommerceMobileApp
+//
+//  Created by Raphael Cerqueira on 03/11/20.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
